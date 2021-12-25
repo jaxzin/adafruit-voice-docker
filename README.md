1d69fcb120c4e038831a06cabd15c2fd1642fe1f
Brian R. Jackson <brian@jaxzin.com>
2021-12-25 23:32:39 +0000

    First working version of the white-noise container.

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 ### Tested Hardware
 
-* Raspberry Pi Zero W
+* Raspberry Pi Zero W Rev1.1
+* Adafruit Voice Bonnet
 
 ### Prerequisites
 
+* Install Raspberry Pi OS Lite to an SD Card (out-of-scope for this doc)
+* Setup WiFi or connect a monitor and keyboard (out-of-scope for this doc)
 * Install Docker & Docker Compose (TBD)
-* For the speaker & mic demos, enable I2C support: `sudo raspi-config nonint do_i2c 0`
+* For the OLED demo, enable I2C support: `sudo raspi-config nonint do_i2c 0`
+* For the speaker demo, [install the seeed-voicecard](https://learn.adafruit.com/adafruit-voice-bonnet/audio-setup)
---
+++ white-noise/white-noise.py
@@ -0,0 +1,56 @@
+from time import sleep
+import soothingsounds as ss
+from argparse import ArgumentParser
+import signal
+
+soundmod = "sounddevice"  # 'pygame'#'pyglet'#'pyaudio' #'pygame' #'scikits.audiolab'
+
+nbitfile = 16
+nbitfloat = 32  # from generator.py
+
+# Setup exit handler
+kill_now = False
+def exit_handler(signum, frame):
+    kill_now = True
+
+signal.signal(signal.SIGINT, exit_handler)
+signal.signal(signal.SIGTERM, exit_handler)
+
+def main():
+    P = ArgumentParser(
+        description="noise generation program for Raspberry Pi or any Python-capable computer"
+    )
+    P.add_argument(
+        "nmode",
+        help="what type of white noise [white, pink, brown...]",
+        nargs="?",
+        default="pink",
+    )
+    P.add_argument(
+        "hours",
+        help="how many hours do you want sound generated for [default=1 hour]",
+        type=float,
+        nargs="?",
+        default=1,
+    )
+    P.add_argument(
+        "-fs", help="sampling freq e.g. 16000 or 44100", type=int, default=16000
+    )
+    P.add_argument(
+        "-nsec",
+        help="length of unique noise sequence [seconds]",
+        type=float,
+        default=60,
+    )
+    p = P.parse_args()
+
+    samps = ss.computenoise(p.nmode, p.fs, p.nsec, nbitfloat, nbitfile)
+
+    while not kill_now:
+        ss.liveplay(samps, p.hours, p.fs, p.nsec, soundmod)
+        sleep(p.hours * 3600)  # for async playback, else sound doesn't play
+
+if __name__ == "__main__":
+    main()
+
+
